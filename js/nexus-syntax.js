dbeb648ae049709cec83f12426c55d9bf359914d
AudricHarris <audricharrismail@gmail.com>
2026-06-28 00:01:02 +0200

    Added Syntax highlighting

---
+++ js/nexus-syntax.js
@@ -0,0 +1,6 @@
+Prism.languages.nexus = Prism.languages.extend('clike', {
+    'keyword': /\b(fn|struct|enum|match|public|private|static|const|new|range|return|if|else|while|true|false)\b/,
+    'type': /\b(i32|i64|f32|f64|bool|str)\b/
+});
+
+Prism.languages.nexus['punctuation'] = /->|[{}[\];(),.:]/;
