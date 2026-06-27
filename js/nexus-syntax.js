f002a54ee4deec64ab746f45130b9cf908f677dc
AudricHarris <audricharrismail@gmail.com>
2026-06-28 00:05:49 +0200

    Updated syntax colouring

--- js/nexus-syntax.js
+++ js/nexus-syntax.js
@@ -1,3 +1,5 @@
+Prism.languages.nexus = {};
+
 Prism.languages.nexus = Prism.languages.extend('clike', {
     'keyword': /\b(fn|struct|enum|match|public|private|static|const|new|range|return|if|else|while|true|false)\b/,
     'type': /\b(i32|i64|f32|f64|bool|str)\b/
